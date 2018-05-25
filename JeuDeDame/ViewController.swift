2ad7d04b1e32ed53f7bbb7f9a0b7184fbb10c64a
Maxime COUPAS <macoupas@iutclinfa1007m.local>
2018-05-25 14:42:03 +0100

    Initial Commit

---
+++ JeuDeDame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JeuDeDame
+//
+//  Created by Maxime COUPAS on 25/05/2018.
+//  Copyright © 2018 Maxime COUPAS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
